ad877d99108e9e89b11c896d3d35bbc1501fc735
victor <victor@ti2-15>
2018-03-29 20:28:40 +0200

    Board fields

---
+++ src/Board.java
@@ -0,0 +1,29 @@
+
+import javax.swing.JPanel;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author victor
+ */
+public class Board extends JPanel {
+    
+    public static final int NUM_ROWS = 22;
+    public static final int NUM_COLS = 10;
+    
+    private Tetrominoes[][] matrix;
+    private int deltaTime;
+    
+    private Shape currentShape;
+    
+    private int currentRow;
+    private int currentCol;
+    
+    
+    
+}
